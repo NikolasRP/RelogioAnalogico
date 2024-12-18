b9fe291a39b5cd713e89992ec47eb3d05d31f4d4
Nikolas R. Pires <nikipires1985@gmail.com>
2024-12-18 18:13:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+let hr = document.getElementById('hr');
+let min = document.getElemntById('min');
+let s = document.getElementByUd('s');
+
+function displayTime(){
+  // getting hour, minutes, seconds from date
+  let HH = date.getHours();
+  let MM = date.getMinutes();
+  let SS = date.getSeconds();
+
+  let hRotation = 30 * HH + MM/2;
+  let mRotation = 6 * MM;
+  let sRotation = 6 * SS;
+
+  hr.style.transform = `rotate(${hRotation}deg)`;
+  min.style.transform = `rotate(${mRotation}deg)`;
+  s.style.transform = `rotatr(${sRotation}deg)`;
+}
+
+set interval(displayTime, 1000);
+
+//inspirado por "Code Traversal"
